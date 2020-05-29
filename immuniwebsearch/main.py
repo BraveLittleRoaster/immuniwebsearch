e4b8948c81416f3157c48122aada65a355d9e827
null <null@null.null>
2020-05-28 23:01:28 -0700

    Fixed a couple of minor issues.

--- immuniwebsearch/main.py
+++ immuniwebsearch/main.py
@@ -291,11 +291,22 @@ class Scraper:
             self.dump_json(all_results)
 
 
-def which(file):
-    for path in os.environ["PATH"].split(os.pathsep):
-        if os.path.exists(os.path.join(path, file)):
-                return os.path.join(path, file)
+def which(program):
 
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 def main():
 
